26ae76f964b458fbb9e5d9c7c62d8df891a399cc
Hannah Thorley <hannahthorley2014@gmail.com>
2023-11-21 08:28:30 +0000

    styling character card

--- src/components/character-profile/CharacterProfile.jsx
+++ src/components/character-profile/CharacterProfile.jsx
@@ -4,25 +4,30 @@ import { SearchFilterContext } from "../wizard-search/WizardSearch";
 const CharacterProfile = () => {
   const { selectedCharacter } = useContext(SearchFilterContext);
   console.log({ selectedCharacter });
-  return selectedCharacter ? (
-    <>
-      <div className="w-[400px] border rounded-md border-black p-2">
-        Character Profile
-        <p>{selectedCharacter.name}</p>
-        <img src={selectedCharacter.image}></img>
-        {selectedCharacter.house && <p>House: {selectedCharacter.house}</p>}
-        {selectedCharacter.patronus && (
-          <p>Patronus: {selectedCharacter.patronus}</p>
-        )}
-        {selectedCharacter.wand.core && (
-          <p>Wand Core: {selectedCharacter.wand?.core}</p>
-        )}
-      </div>
-    </>
-  ) : (
-    <p className="w-[400px] border rounded-md border-black p-2">
-      Select your character on the left
-    </p>
+  return (
+    <div className="w-[300px] h-[400px] mr-5 border rounded-lg bg-black text-white p-2 ">
+      {selectedCharacter ? (
+        <div className="flex items-center flex-col">
+          <h2 className="text-xl font-medium">Character Profile</h2>
+          <p className="font-medium">{selectedCharacter.name}</p>
+          <img
+            className="h-[230px] rounded-lg my-4"
+            src={selectedCharacter.image}
+          ></img>
+          {selectedCharacter.house && <p>House: {selectedCharacter.house}</p>}
+          {selectedCharacter.patronus && (
+            <p className="font-light">Patronus: {selectedCharacter.patronus}</p>
+          )}
+          {selectedCharacter.wand.core && (
+            <p className="font-light">
+              Wand Core: {selectedCharacter.wand?.core}
+            </p>
+          )}
+        </div>
+      ) : (
+        <p>Select your character on the left</p>
+      )}
+    </div>
   );
 };
 
